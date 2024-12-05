4dc9ec8977eae1d816daa9d9524df3cbb1944948
Wszerad <wmartynowski@divante.pl>
2024-12-05 22:52:18 +0100

    Initial commit

---
+++ components/Cat.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useId } from 'vue'
+
+const id = useId()
+
+defineProps<{ data: number | null }>()
+</script>
+
+<template>
+<span :id="id">
+  {{ data }}
+</span>
+</template>
---
+++ pages/cat.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Cat from '../components/Cat.vue'
+
+const { data } = useAsyncData<number>('ss', async () => {
+  return new Promise<number>((resolve, reject) => {
+    setTimeout(() => resolve(2), 100)
+  })
+})
+</script>
+
+<template>
+  <div class="page">
+    <Cat :data="data" />
+  </div>
+</template>
